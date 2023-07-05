46d2d91d903b4233f3a4b8372dec93b982b71e7d
itsme1502 <harshitshankrit@gmail.com>
2023-07-05 23:11:40 +0530

    Sockets

--- server.js
+++ server.js
@@ -19,20 +19,6 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
 app.use(notFound);
 app.use(errorHandler);
 
@@ -46,7 +32,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://talk-nation.vercel.app"]
   },
 });
 
